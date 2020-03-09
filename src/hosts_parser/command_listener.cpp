62ff52c6d47f3156c3b9d20d5b3eb37cb0bae7c4
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2020-03-09 23:50:52 +0100

    Implement command_listener

--- src/hosts_parser/command_listener.cpp
+++ src/hosts_parser/command_listener.cpp
@@ -1,31 +1,52 @@
-//
-// Created by Enrico Maria Crisostomo on 06/03/2020.
-//
+/*
+ * Copyright (c) 2020 Enrico M. Crisostomo
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "command_listener.h"
 
-void command_listener::visitErrorNode(antlr4::tree::ErrorNode *node)
-{
-  is_error = true;
-}
-
-//void command_listener::exitSet_command(hosts::Set_commandContext *context)
-//{
-//  command = hostage_command::SET;
-//}
-
 void command_listener::exitAddress(hosts::AddressContext *context)
 {
-  addresses.push_back(context->getText());
+  cmd.addresses.push_back(context->getText());
 }
 
 void command_listener::exitHost_name(hosts::Host_nameContext *context)
 {
-  host_names.push_back(context->getText());
+  cmd.host_names.push_back(context->getText());
 }
 
-//void command_listener::exitRm_command(hosts::Rm_commandContext *context)
-//{
-//  command = hostage_command::RM_HOST;
-//}
+void command_listener::exitRm_host_command(hosts::Rm_host_commandContext *context)
+{
+  cmd.command = hostage_command::RM_HOST;
+}
 
+void command_listener::exitRm_address_command(hosts::Rm_address_commandContext *context)
+{
+  cmd.command = hostage_command::RM_ADDRESS;
+}
+
+void command_listener::exitSet_command(hosts::Set_commandContext *context)
+{
+  cmd.command = hostage_command::SET;
+}
+
+command command_listener::get_command() const
+{
+  return cmd;
+}
+
+void command_listener::visitErrorNode(antlr4::tree::ErrorNode *node)
+{
+  cmd.error = true;
+}
--- src/hosts_parser/command_listener.h
+++ src/hosts_parser/command_listener.h
@@ -1,6 +1,18 @@
-//
-// Created by Enrico Maria Crisostomo on 06/03/2020.
-//
+/*
+ * Copyright (c) 2020 Enrico M. Crisostomo
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef HOSTAGE_COMMAND_LISTENER_H
 #define HOSTAGE_COMMAND_LISTENER_H
@@ -18,20 +30,26 @@ enum class hostage_command
   RM_ADDRESS
 };
 
+struct command
+{
+  hostage_command command{hostage_command::UNSET};
+  bool error;
+  std::vector<std::string> addresses;
+  std::vector<std::string> host_names;
+};
+
 class command_listener : public hostsBaseListener
 {
 public:
+  void exitRm_address_command(hosts::Rm_address_commandContext *context) override;
+  void exitRm_host_command(hosts::Rm_host_commandContext *context) override;
+  void exitSet_command(hosts::Set_commandContext *context) override;
   void exitAddress(hosts::AddressContext *context) override;
   void exitHost_name(hosts::Host_nameContext *context) override;
-//  void exitRm_command(hosts::Rm_commandContext *context) override;
-//  void exitSet_command(hosts::Set_commandContext *context) override;
   void visitErrorNode(antlr4::tree::ErrorNode *node) override;
-
+  command get_command() const;
 private:
-  bool is_error;
-  hostage_command command;
-  std::vector<std::string> addresses;
-  std::vector<std::string> host_names;
+  command cmd{};
 };
 
 
